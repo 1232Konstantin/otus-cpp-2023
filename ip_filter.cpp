3946ce270b3ae12579647fe43e4596ed6aad7585
Daulet <daulettbot@gmail.com>
2023-05-11 14:43:58 +0600

    fix according to the review

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -1,29 +1,39 @@
-#include <tuple>
 #include <string>
 #include <array>
 #include <vector>
 #include <algorithm>
 #include <iostream>
 
-typedef std::tuple<uint8_t, uint8_t, uint8_t, uint8_t> IpAddress;
+typedef std::array<uint8_t, 4> IpAddress;
 
 IpAddress ParseIpAddress(const std::string& AddressStr)
 {
   constexpr char ADDRESS_DELIM = '.';
 
-  std::array<uint8_t, std::tuple_size<IpAddress>::value> TempContainer{};
+  IpAddress Address{};
   std::string::size_type Start = 0;
   auto Stop = AddressStr.find_first_of(ADDRESS_DELIM);
 
-  for (uint8_t i = 0; i < TempContainer.size(); ++i)
+  for (uint8_t i = 0; i < Address.size(); ++i)
   {
-    TempContainer[i] = static_cast<uint8_t>(std::stoi(AddressStr.substr(Start, Stop - Start)));
+    try
+    {
+      Address[i] = static_cast<uint8_t>(std::stoi(AddressStr.substr(Start, Stop - Start)));
+    }
+    catch (std::exception const& ex)
+    {
+      std::cerr << "Failed to parse: '" << AddressStr.substr(Start, Stop - Start)
+        << "' which is a part of the string: '" << AddressStr << "' Start = "
+        << Start << " Stop = " << Stop << '\n' << ex.what() << std::endl;
+      Address = {};
+      break;
+    }
+    
     Start = Stop + 1;
     Stop = AddressStr.find_first_of(ADDRESS_DELIM, Start);
   }
-
-  // need some template magic to cast array or vector to tuple
-  return { TempContainer[0], TempContainer[1], TempContainer[2], TempContainer[3] };
+  
+  return Address;
 }
 
 std::istream& operator>>(std::istream& is, std::vector<IpAddress>& IpPool)
@@ -42,10 +52,10 @@ std::istream& operator>>(std::istream& is, std::vector<IpAddress>& IpPool)
 
 std::ostream& operator<<(std::ostream& os, const IpAddress& Ip)
 {
-  os << static_cast<int>(std::get<0>(Ip)) << '.' 
-    << static_cast<int>(std::get<1>(Ip)) << '.'
-    << static_cast<int>(std::get<2>(Ip)) << '.'
-    << static_cast<int>(std::get<3>(Ip));
+  os << static_cast<int>(Ip[0]) << '.'
+    << static_cast<int>(Ip[1]) << '.'
+    << static_cast<int>(Ip[2]) << '.'
+    << static_cast<int>(Ip[3]);
   return os;
 }
 
@@ -78,7 +88,7 @@ int main()
     decltype(IpPool) FilteredIpPool;
     std::copy_if(IpPool.begin(), IpPool.end(), std::back_inserter(FilteredIpPool), [](const auto& Elem)
       {
-        return std::get<0>(Elem) == 1;
+        return Elem[0] == 1;
       }
     );
     std::cout << FilteredIpPool << std::endl;
@@ -89,7 +99,7 @@ int main()
     decltype(IpPool) FilteredIpPool;
     std::copy_if(IpPool.begin(), IpPool.end(), std::back_inserter(FilteredIpPool), [](const auto& Elem)
       {
-        return std::get<0>(Elem) == 46 && std::get<1>(Elem) == 70;
+        return Elem[0] == 46 && Elem[1] == 70;
       }
     );
     std::cout << FilteredIpPool << std::endl;
@@ -100,7 +110,11 @@ int main()
     decltype(IpPool) FilteredIpPool;
     std::copy_if(IpPool.begin(), IpPool.end(), std::back_inserter(FilteredIpPool), [](const auto& Elem)
       {
-        return std::get<0>(Elem) == 46 || std::get<1>(Elem) == 46 || std::get<2>(Elem) == 46 || std::get<3>(Elem) == 46;
+        for (const auto& Part : Elem)
+        {
+          if (Part == 46) return true;
+        }
+        return false;
       }
     );
     std::cout << FilteredIpPool << std::endl;
