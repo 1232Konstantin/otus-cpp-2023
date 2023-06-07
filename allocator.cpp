1b8aaba8375592cfebbf0cea8fd2359c2015d211
Daulet Tumbayev <daulettbot@gmail.com>
2023-06-07 23:13:38 +0600

    timus done

---
+++ allocator.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "todo allocator logic" << std::endl;
+  return 0;
+}
---
+++ timus.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+namespace Timus
+{
+  void task1409(std::istream& in, std::ostream& out);
+  void task1877(std::istream& in, std::ostream& out);
+  void task2001(std::istream& in, std::ostream& out);
+  void task1264(std::istream& in, std::ostream& out);
+  void task1787(std::istream& in, std::ostream& out);
+}
