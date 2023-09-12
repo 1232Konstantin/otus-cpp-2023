60a95215438e51811cbe9222e96d60c4260a8ff8
Daulet Tumbayev <daulettbot@gmail.com>
2023-09-12 23:31:15 +0600

    draft

--- main.cpp
+++ main.cpp
@@ -6,70 +6,121 @@
 
 int main()
 {
-    Matrix<int, 0> mat;
+  Matrix<int, 0> mat;
 
-    for (int i = 0; i < 10; ++i)
+  for (int i = 0; i < 10; ++i)
+  {
+    mat[i][i] = i;
+    mat[i][9 - i] = 9 - i;
+  }
+
+  for (int i = 1; i < 9; ++i)
+  {
+    for (int j = 1; j < 9; ++j)
     {
-        mat[i][i] = i;
-        mat[i][9 - i] = 9 - i;
+      std::cout << mat[i][j] << ' ';
     }
+    std::cout << '\n';
+  }
+  std::cout << std::endl;
 
-    for (int i = 1; i < 9; ++i)
-    {
-        for (int j = 1; j < 9; ++j)
-        {
-            std::cout << mat[i][j] << ' ';
-        }
-        std::cout << '\n';
-    }
-    std::cout << std::endl;
-    
-    std::cout << "Size = " << mat.size() << std::endl;
-    std::cout << std::endl;
+  std::cout << "Size = " << mat.size() << std::endl;
+  std::cout << std::endl;
 
-    for (const auto& c : mat)
-    {
-        int x, y, v;
-        std::tie(x, y, v) = c;
-        std::cout << "X = " << x << " Y = " << y << " V = " << v << '\n'; 
-    }
-    std::cout << std::endl;
+  for (const auto& c : mat)
+  {
+    int x, y, v;
+    std::tie(x, y, v) = c;
+    std::cout << "X = " << x << " Y = " << y << " V = " << v << '\n';
+  }
+  std::cout << std::endl;
 
-    return 0;
+  return 0;
 }
 #endif
 
 #include <array>
 #include <map>
 #include <tuple>
+#include <memory>
 
-template <size_t Depth>
+using KeyT = std::array<size_t, 3>;
+using StorageT = std::map<KeyT, int>;
+using StorageItT = typename StorageT::iterator;
+
+template <size_t OriginalDepth, size_t CurrentDepth>
 class Level
 {
 public:
-    static constexpr bool val = true;
-    Level<Depth - 1> get()
-    {
-        return Level<Depth - 1>();
-    }
+  static constexpr bool isBottom = false;
+
+  KeyT Key;
+  std::shared_ptr<StorageT> Storage;
+  
+  Level(const int& inIndex, KeyT&& inKey, std::shared_ptr<StorageT> inStorage)
+    : Key(std::move(inKey))
+    , Storage(std::move(inStorage))
+  {
+    Key[OriginalDepth - CurrentDepth] = inIndex;
+  }
+
+  Level<OriginalDepth, CurrentDepth - 1>& operator[](const int& index)
+  {
+    return Level<OriginalDepth, CurrentDepth - 1>(index, std::move(Key), Storage);
+  }
 };
 
-template <>
-class Level<1>
+template <size_t OriginalDepth>
+class Level<OriginalDepth, 2>
 {
 public:
-    static constexpr bool val = false;
+  static constexpr bool isBottom = true;
+
+  KeyT Key;
+  std::shared_ptr<StorageT> Storage;
+
+  Level(const int& inIndex, KeyT&& inKey, std::shared_ptr<StorageT> inStorage)
+    : Key(std::move(inKey))
+    , Storage(std::move(inStorage))
+  {
+    Key[OriginalDepth - 2] = inIndex;
+  }
+
+  int& operator[](const int& index)
+  {
+    Key[OriginalDepth - 1] = index;
+    return Storage->operator[](Key);
+  }
+};
+
+class Mat
+{
+public:
+
+  std::shared_ptr<StorageT> Storage;
+
+  Mat()
+  {
+    Storage = std::make_shared<StorageT>();
+    Storage->insert({ KeyT{ 0,1,2 }, 42 });
+  }
+
+  Level<3, 3>& operator[](const int& index)
+  {
+    return Level<3, 3>(index, {}, Storage);
+  }
 };
 
 
 int main()
 {
-    Level<3> l3;
-    auto vl3 = l3.val;
-    auto l2 = l3.get();
-    auto vl2 = l2.val;
-    auto l1 = l2.get();
-    auto vl1 = l1.val;
+  Mat m;
 
-    return 0;
+  //auto& l1 = m[11];
+  //auto& l2 = m[11][22];
+  auto& l3 = m[11][22][33];
+
+  m[11][22][33] = 42;
+
+  return 0;
 }
--- matrix.h
+++ matrix.h
@@ -3,103 +3,129 @@
  * @author Dabudabot
  */
 
-#include <unordered_map>
+#include <map>
 #include <array>
 
 template <class T, T DefaultValue, size_t Dimentions = 2>
 class Matrix
 {
 private:
-    using KeyT = std::array<size_t, Dimentions>;
-    using StorageT = std::unordered_map<KeyT, T>;
-    using StorageItT = typename StorageT::iterator;
+  using KeyT = std::array<size_t, Dimentions>;
+  using StorageT = std::map<KeyT, T>;
+  using StorageItT = typename StorageT::iterator;
 
 private:
 
-    // https://www.artificialworlds.net/blog/2017/05/12/c-iterator-wrapperadaptor-example/
-    // https://www.internalpointers.com/post/writing-custom-iterators-modern-cpp
-    class MatrixIt
+  // https://www.artificialworlds.net/blog/2017/05/12/c-iterator-wrapperadaptor-example/
+  // https://www.internalpointers.com/post/writing-custom-iterators-modern-cpp
+  class MatrixIt
+  {
+  private:
+
+    StorageItT Wrapped;
+
+  public:
+    explicit MatrixIt(StorageItT inWrapped) : Wrapped(inWrapped) {}
+
+    auto operator*()
     {
-    private:
+      // https://stackoverflow.com/questions/57510341/how-to-convert-stdarray-to-stdtuple
+      // https://en.cppreference.com/w/cpp/utility/tuple/tuple_cat
+      return std::tuple_cat(Wrapped->first, std::make_tuple(Wrapped->second));
+    }
 
-        StorageItT Wrapped;
-
-    public:
-        explicit MatrixIt(StorageItT inWrapped) : Wrapped(inWrapped) {}
-
-        auto operator*()
-        {
-            // https://stackoverflow.com/questions/57510341/how-to-convert-stdarray-to-stdtuple
-            // https://en.cppreference.com/w/cpp/utility/tuple/tuple_cat
-            return std::tuple_cat(Wrapped->first, std::make_tuple(Wrapped->second));
-        }
-
-        // same as in original iterator
-        bool operator==(const MatrixIt& other) const { return Wrapped.operator==(other); }
-        bool operator!=(const MatrixIt& other) const { return Wrapped.operator!=(other); }
-        MatrixIt& operator++()
-        {
-            ++Wrapped;
-            return *this;
-        }
-        MatrixIt& operator++(int)
-        {
-            auto temp = *this;
-            ++Wrapped;
-            return temp;
-        }
-        MatrixIt& operator--()
-        {
-            --Wrapped;
-            return *this;
-        }
-        MatrixIt& operator--(int)
-        {
-            auto temp = *this;
-            --Wrapped;
-            return temp;
-        }
-    };
-
-    template <class Depth>
-    class Level
+    // same as in original iterator
+    bool operator==(const MatrixIt& other) const { return Wrapped.operator==(other); }
+    bool operator!=(const MatrixIt& other) const { return Wrapped.operator!=(other); }
+    MatrixIt& operator++()
     {
-    private:
-
-        KeyT Key;
-
-    };
-
-    template <>
-    class Level<1>
+      ++Wrapped;
+      return *this;
+    }
+    MatrixIt& operator++(int)
     {
+      auto temp = *this;
+      ++Wrapped;
+      return temp;
+    }
+    MatrixIt& operator--()
+    {
+      --Wrapped;
+      return *this;
+    }
+    MatrixIt& operator--(int)
+    {
+      auto temp = *this;
+      --Wrapped;
+      return temp;
+    }
+  };
 
-    };
+  class Level<size_t OriginalDepth, size_t CurrentDepth>
+  {
+    KeyT Key;
+    std::shared_ptr<StorageT> Storage;
+
+    Level(const int& inIndex, KeyT&& inKey, std::shared_ptr<StorageT> inStorage)
+      : Key(std::move(inKey))
+      , Storage(std::move(inStorage))
+    {
+      Key[OriginalDepth - CurrentDepth] = inIndex;
+    }
+
+    Level<OriginalDepth, CurrentDepth - 1>& operator[](const int& index)
+    {
+      return Level<OriginalDepth, CurrentDepth - 1>(index, std::move(Key), Storage);
+    }
+  };
+
+  template <size_t OriginalDepth>
+  class Level<OriginalDepth, 2>
+  {
+  public:
+    static constexpr bool isBottom = true;
+
+    KeyT Key;
+    std::shared_ptr<StorageT> Storage;
+
+    Level(const int& inIndex, KeyT&& inKey, std::shared_ptr<StorageT> inStorage)
+      : Key(std::move(inKey))
+      , Storage(std::move(inStorage))
+    {
+      Key[OriginalDepth - 2] = inIndex;
+    }
+
+    T& operator[](const int& index)
+    {
+      Key[OriginalDepth - 1] = index;
+      return Storage->operator[](Key);
+    }
+  };
 
 private:
 
-    StorageT Storage;
+  StorageT Storage;
 
 public:
 
-    auto operator[](size_t index)
-    {
-        return Level<Dimentions>(Storage, index);
-    }
+  Level<Dimentions, Dimentions>& operator[](size_t index)
+  {
+    return Level<Dimentions, Dimentions>(index, {}, Storage);
+  }
 
-    std::size_t size() const
-    {
-        return Storage.size();
-    }
+  std::size_t size() const
+  {
+    return Storage.size();
+  }
 
-    MatrixIt begin()
-    {
-        return MatrixIt(Storage.begin());
-    }
+  MatrixIt begin()
+  {
+    return MatrixIt(Storage.begin());
+  }
 
-    MatrixIt end()
-    {
-        return MatrixIt(Storage.end());
-    }
-    
+  MatrixIt end()
+  {
+    return MatrixIt(Storage.end());
+  }
+
 };
